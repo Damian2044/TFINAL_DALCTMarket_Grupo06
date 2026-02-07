b7941bf1ba1b9c79a753097d5417ab187cbee2a4
SthalinCha <dambarguz.2004@gmail.com>
2026-02-07 11:02:26 -0500

    frontend: agregar configuración base del proyecto

--- frontend/app-frontend/src/main.jsx
+++ frontend/app-frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' 
+import { JwtProvider } from '@/context/jwtContext'
 import './index.css' // Importar el archivo CSS global (Tailwind CSS)
-import App from './App.jsx'
+import Rutas from './Rutas'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
+ 
+  <StrictMode>
+      <BrowserRouter>
+        <JwtProvider>
+      <Rutas />
+        </JwtProvider>
+      </BrowserRouter>
+  </StrictMode>
+ 
 )
